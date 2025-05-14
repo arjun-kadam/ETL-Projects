c93d8aa7e025616ee02a76907330afe8cdde014b
Arjun Kadam <arjunkadama29@gmail.com>
2025-05-14 22:33:33 +0530

    added mysql helper

--- ftpToMySQL/src/main/java/com/etl/ftp2mysql/services/impl/DataPersistenceWorkerServiceImpl.java
+++ ftpToMySQL/src/main/java/com/etl/ftp2mysql/services/impl/DataPersistenceWorkerServiceImpl.java
@@ -1,8 +1,11 @@
 package com.etl.ftp2mysql.services.impl;
 
+import com.etl.ftp2mysql.payload.MySQLCredentialDTO;
 import com.etl.ftp2mysql.repository.DataProcessingLogRepository;
+import com.etl.ftp2mysql.repository.MySQLCredentialRepository;
 import com.etl.ftp2mysql.services.DataPersistenceWorkerService;
 import com.etl.ftp2mysql.services.DataProcessingLogService;
+import com.etl.ftp2mysql.utilities.MySQLCredentialHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +18,12 @@ public class DataPersistenceWorkerServiceImpl implements DataPersistenceWorkerSe
     @Autowired
     private DataProcessingLogService dataProcessingLogService;
 
+    @Autowired
+    private MySQLCredentialRepository mySQLCredentialRepository;
+
     @Override
     public void persistDataToExternalMySQLClient() {
+        MySQLCredentialDTO mySQLCredentials= MySQLCredentialHelper.getMySQLCredentials(1L,mySQLCredentialRepository);
 
     }
 }
--- ftpToMySQL/src/main/java/com/etl/ftp2mysql/utilities/MySQLCredentialHelper.java
+++ ftpToMySQL/src/main/java/com/etl/ftp2mysql/utilities/MySQLCredentialHelper.java
@@ -1,4 +1,23 @@
 package com.etl.ftp2mysql.utilities;
 
+import com.etl.ftp2mysql.entity.MySQLCredential;
+import com.etl.ftp2mysql.payload.MySQLCredentialDTO;
+import com.etl.ftp2mysql.repository.MySQLCredentialRepository;
+
 public class MySQLCredentialHelper {
+    private MySQLCredentialHelper() {
+    }
+
+    public static MySQLCredentialDTO getMySQLCredentials(Long id, MySQLCredentialRepository repo) {
+        MySQLCredential mySQLCredential = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Credentials not found"));
+
+        MySQLCredentialDTO dto = new MySQLCredentialDTO();
+        dto.setHost(mySQLCredential.getHost());
+        dto.setDatabaseName(mySQLCredential.getDatabaseName());
+        dto.setUserName(mySQLCredential.getUserName());
+        dto.setPassword(mySQLCredential.getPassword());
+        dto.setPort(mySQLCredential.getPort());
+        return dto;
+    }
 }
